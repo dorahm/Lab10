468d468c04cc7f6fda3a3a038550ed79803b1145
dorahm <159638043+dorahm@users.noreply.github.com>
2024-04-13 20:51:00 -0400

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+// SelectionSort.java
+public class SelectionSort {
+    private int temp;
+
+    public SelectionSort() {
+    }
+
+    public int[] basicSelectionSort(int[] x) {
+        for (int i = 0; i < x.length; ++i) {
+            for (int j = i + 1; j < x.length; ++j) {
+                if (x[i] > x[j]) {
+                    temp = x[i];
+                    x[i] = x[j];
+                    x[j] = temp; // Corrected: should assign temp to x[j]
+                }
+            }
+        }
+        return x;
+    }
+}
---
+++ testSelectionSort.java
@@ -0,0 +1,76 @@
+// testSelectionSort.java
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testSelectionSort {
+    @Test
+    public void test() {
+        testPositive();
+        testNegative();
+        testMixed();
+        testDuplicates();
+    }
+
+    public testSelectionSort() {
+    }
+
+    public void testPositive() {
+        int[] arr = new int[] { 8, 9, 7, 10, 2 };
+        SelectionSort selectionSort = new SelectionSort();
+        int[] sortedArr = selectionSort.basicSelectionSort(arr);
+        int[] expectedArr = new int[] { 2, 7, 8, 9, 10 };
+        assertArrayEquals(expectedArr, sortedArr);
+
+        // Print sorted array
+        System.out.println("Sorted Array (testPositive):");
+        for (int num : sortedArr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public void testNegative() {
+        int[] arr = new int[] { -8, -9, -7, -10, -2 };
+        SelectionSort selectionSort = new SelectionSort();
+        int[] sortedArr = selectionSort.basicSelectionSort(arr);
+        int[] expectedArr = new int[] { -10, -9, -8, -7, -2 };
+        assertArrayEquals(expectedArr, sortedArr);
+
+        // Print sorted array
+        System.out.println("Sorted Array (testNegative):");
+        for (int num : sortedArr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public void testMixed() {
+        int[] arr = new int[] { -8, 9, 0, -7, 10, 2, 0 };
+        SelectionSort selectionSort = new SelectionSort();
+        int[] sortedArr = selectionSort.basicSelectionSort(arr);
+        int[] expectedArr = new int[] { -8, -7, 0, 0, 2, 9, 10 };
+        assertArrayEquals(expectedArr, sortedArr);
+
+        // Print sorted array
+        System.out.println("Sorted Array (testMixed):");
+        for (int num : sortedArr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public void testDuplicates() {
+        int[] arr = new int[] { 8, 9, 7, 10, 2, 8, 9, 7, 10, 2 };
+        SelectionSort selectionSort = new SelectionSort();
+        int[] sortedArr = selectionSort.basicSelectionSort(arr);
+        int[] expectedArr = new int[] { 2, 2, 7, 7, 8, 8, 9, 9, 10, 10 };
+        assertArrayEquals(expectedArr, sortedArr);
+
+        // Print sorted array
+        System.out.println("Sorted Array (testDuplicates):");
+        for (int num : sortedArr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
